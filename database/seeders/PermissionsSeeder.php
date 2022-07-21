10c56c337a93f542d98dbeb3be01bfc7b1cfb80a
chambokk <chamae12anecito@gmail.com>
2022-07-21 13:31:12 +0800

    vehicle subtask

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -18,10 +18,11 @@ class PermissionsSeeder extends Seeder
             array('id' => 1,'permission_name' => 'can_edit_user'),
             array('id' => 2,'permission_name' => 'can_create_user'),
             array('id' => 3,'permission_name' => 'can_delete_user'),
-            array('id' => 4,'permission_name' => 'can_create_vehicle'), 
+            array('id' => 4,'permission_name' => 'can_create_vehicle'), // GSO
             array('id' => 5,'permission_name' => 'can_create_travel'),  //RO
-            array('id' => 6,'permission_name' => 'can_set_status'), //PG-Head
+            array('id' => 6,'permission_name' => 'can_set_status'), // PG-Head
             array('id' => 7,'permission_name' => 'can_merge_travel'),
+            array('id' => 8,'permission_name' => 'can_merge_driver'), // GSO
         );
     }
 }
