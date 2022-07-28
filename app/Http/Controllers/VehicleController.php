6b82a4cea08f2121cc8fe73a2ec2a09f820f71dd
chambokk <chamae12anecito@gmail.com>
2022-07-28 19:30:10 +0800

    update

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -136,7 +136,7 @@ class VehicleController extends Controller
                 )
                 ->leftJoin('vehicle_status', 'vehicle_status.vehicles_id', 'vehicles.id')
                 ->leftJoin('driver_vehicles', 'vehicles_id', 'id')
-                ->when(,'driver_vehicles.department_code', auth()->user()->office_id)
+                ->when('driver_vehicles.department_code', auth()->user()->office_id)
                 // ->where(function ($query) use($id){
                 //     $query->where('vehicle_status.condition', 'Good Condition')
                 //         ->orWhere('vehicle_status.vehicles_id', $id);
