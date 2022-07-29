d865b41374e13e1daa54686e53a9a2604409fa7e
chambokk <chamae12anecito@gmail.com>
2022-07-29 14:05:39 +0800

    changes

--- public/js/app.js
+++ public/js/app.js
@@ -2005,16 +2005,6 @@ var map = {
 		9,
 		"resources_js_Pages_Users_Settings_vue"
 	],
-	"./VehicleStatus": [
-		"./resources/js/Pages/VehicleStatus/index.vue",
-		9,
-		"resources_js_Pages_VehicleStatus_index_vue"
-	],
-	"./VehicleStatus/": [
-		"./resources/js/Pages/VehicleStatus/index.vue",
-		9,
-		"resources_js_Pages_VehicleStatus_index_vue"
-	],
 	"./VehicleStatus/Create": [
 		"./resources/js/Pages/VehicleStatus/Create.vue",
 		9,
@@ -2025,6 +2015,16 @@ var map = {
 		9,
 		"resources_js_Pages_VehicleStatus_Create_vue"
 	],
+	"./VehicleStatus/Index": [
+		"./resources/js/Pages/VehicleStatus/Index.vue",
+		9,
+		"resources_js_Pages_VehicleStatus_Index_vue"
+	],
+	"./VehicleStatus/Index.vue": [
+		"./resources/js/Pages/VehicleStatus/Index.vue",
+		9,
+		"resources_js_Pages_VehicleStatus_Index_vue"
+	],
 	"./VehicleStatus/Status": [
 		"./resources/js/Pages/VehicleStatus/Status.vue",
 		9,
@@ -2034,16 +2034,6 @@ var map = {
 		"./resources/js/Pages/VehicleStatus/Status.vue",
 		9,
 		"resources_js_Pages_VehicleStatus_Status_vue"
-	],
-	"./VehicleStatus/index": [
-		"./resources/js/Pages/VehicleStatus/index.vue",
-		9,
-		"resources_js_Pages_VehicleStatus_index_vue"
-	],
-	"./VehicleStatus/index.vue": [
-		"./resources/js/Pages/VehicleStatus/index.vue",
-		9,
-		"resources_js_Pages_VehicleStatus_index_vue"
 	],
 	"./Vehicles/Create": [
 		"./resources/js/Pages/Vehicles/Create.vue",
