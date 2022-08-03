56c89f6fac8b1e733e3bd47f59d04850040a69d5
javarex <raymartnayreitanong11@gmail.com>
2022-08-03 15:03:29 +0800

    user edit permission

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -132,6 +132,24 @@ class UserController extends Controller
             
             $validated['password'] = $data->password;
         }
+
+        if ($request->permission == 'PGO') {
+            //1,2,3 are all available permissions for the admin
+            $data->permissions()->sync([5,6,7,8,10,11,12,13,14,15,16,17,18,19,25,26,31]);
+        } elseif ($request->permission == 'RO') {
+            $data->permissions()->sync([5,7,10,11,12,13,14,15,25]);
+        } elseif ($request->permission == 'PGSO') {
+            $data->permissions()->sync([4,8,16,17,18,19,20,21,22,23,24,27,28,29,30,31]);
+        } elseif ($request->permission == 'PG-Head') {
+            $data->permissions()->sync([6,25,26]);
+        } elseif ($request->permission == 'Admin') {
+            $data->permissions()->sync([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+        } elseif($request->permission == 'peo-motorpool') {
+            $data->permissions()->sync([18,19]);
+        } else {
+            //specify an Array of permissions id here manually
+            $data->permissions()->sync([]);
+        }
         // $data->update([
         //     'name' => $request->name,
         //     'permission' => $request->permission,
