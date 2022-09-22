155f6446f6360828186f2897d317e73f059f034c
jaden14 <JadeCapellan50@gmail.com>
2022-09-22 13:04:34 +0800

    update dashboard balance

--- app/Http/Controllers/ChargeController.php
+++ app/Http/Controllers/ChargeController.php
@@ -223,6 +223,50 @@ class ChargeController extends Controller
     public function balance(Request $request)
     {
 
+        $unpaid = $this->travel->where('status','fueled')->where(function($query) {
+                        $query->where('soa_travel', null)
+                                ->orWhereHas('soa', function ($q) {
+                                    $q->where('cafoa_number', null);
+                                });
+            })->get()->map(function ($item){
+
+                                $checkPrice = $this->prices->where('gasoline_id', $item->gasoline_id)->whereDate('date', $item->date_from)->exists();
+
+                                $total = $this->prices->when($checkPrice, function($q) use ($item) {
+                                                    $q->whereDate('date', $item->date_from);
+                                                })->where('gasoline_id', $item->gasoline_id)
+                                                ->latest()
+                                                ->first($item->gas_type);
+                                $actual = $item->actual_liter ? $item->actual_liter : $item->total_liters;
+          
+                                return [
+                                    'office_id' => $item->office_id,
+                                    'total_liters' => $item->total_liters,
+                                    'price' => number_format($total[$item->gas_type],2),
+                                    'idraao' => $item->idraao,
+                                    'idooe' => $item->idooe,
+                                    "amount" => ($total[$item->gas_type] * $actual),
+                                ]; 
+                            });
+
+        $totalUnpaid = $unpaid->groupBy('idraao')->map(fn($item) => [
+            $item->groupBy('idooe')
+        ])->map(fn($item) => [
+            collect($item)->flatten(1)->map(fn($row1) => [
+                    $row1->mapWithKeys(fn($row) => [
+                        'total_liters' => collect($row1)->sum('total_liters'),
+                        'amount' => collect($row1)->sum('amount'),
+                        'idraao' => $row['idraao'],
+                        'idooe' => $row['idooe'],
+                        'office_id' => $row['office_id']
+
+                    ])
+                
+            ])
+
+        ])->flatten(3);
+       
+
         $paid = $this->travel->whereHas('soa', function ($q) {
                                     $q->where('cafoa_number','!=', null);
                                 })->get()->map(function ($item){
@@ -313,7 +357,7 @@ class ChargeController extends Controller
                     ->where(DB::raw('ooes.fueltag'),'1')
                     ->groupBy(DB::raw('raaods.idraao,raaods.idooe'))
                     ->orderBy(DB::raw('raaohs.ffunccod, raaohs.fraodesc, ooes.fooedesc'))
-                    ->get()->map(function ($items) use ($total,$totalpaid){
+                    ->get()->map(function ($items) use ($total,$totalpaid,$totalUnpaid){
 
                         $balance = $total->filter(function ($value) use ($items) {
                             
@@ -321,6 +365,11 @@ class ChargeController extends Controller
                         })->first();
 
                         $paid = $totalpaid->filter(function ($values) use ($items) {
+                            
+                             return $values['idraao'] == $items->idraao and $values['idooe'] == $items->idooe;
+                        })->first();
+
+                        $unpaid = $totalUnpaid->filter(function ($values) use ($items) {
                             
                              return $values['idraao'] == $items->idraao and $values['idooe'] == $items->idooe;
                         })->first();
@@ -333,7 +382,8 @@ class ChargeController extends Controller
                             'idooe' => $items->idooe,
                             'total_liters' => $balance ? $balance['total_liters']: 0,
                             'amount'=> $balance ? $balance['amount']: 0,
-                            'paid_amount' => $paid ? $paid['amount']: 0
+                            'paid_amount' => $paid ? $paid['amount']: 0,
+                            'unpaid_amount' => $unpaid ? $unpaid['amount'] :0
                         ];
                     });
 
@@ -345,6 +395,7 @@ class ChargeController extends Controller
                 'total_liters' => number_format($items->sum('total_liters'),2),
                 'amount'=> number_format($items->sum('amount'),2),
                 'amount_paid'=> number_format($items->sum('paid_amount'),2),
+                'unpaid_amount'=> number_format($items->sum('unpaid_amount'),2),
             ]
         );
        
